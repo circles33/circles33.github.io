267f2cdfb14e456ec845be721d52e2bf6125b70d
circles33 <51255072+circles33@users.noreply.github.com>
2022-06-06 16:23:38 +0800

    Update print220516.js

--- js/print220516.js
+++ js/print220516.js
@@ -5,6 +5,9 @@ document.getElementById("demo").innerHTML=rand(7,1);
 document.getElementById("demo1").innerHTML=rand(7,1);
 document.getElementById("demo2").innerHTML=rand(9,1);
 document.getElementById("demo3").innerHTML=rand(9,1);
-
-
+if(rand(7,1)>3){
+document.getElementById("if").innerHTML="后";
+}else{
+document.getElementById("if").innerHTML="前";
+}
 
