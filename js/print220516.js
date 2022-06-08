ac08ea9c7de9a7276227dbd03e5ae4dfd134cc21
circles33 <51255072+circles33@users.noreply.github.com>
2022-06-08 22:24:37 +0800

    Update print220516.js

--- js/print220516.js
+++ js/print220516.js
@@ -1,10 +1,34 @@
 function rand(n,m){
 return Math.floor(Math.random()*(n-m)+m);
 };
-document.getElementById("demo").innerHTML=rand(7,1);
-document.getElementById("demo1").innerHTML=rand(7,1);
-document.getElementById("demo2").innerHTML=rand(9,1);
-document.getElementById("demo3").innerHTML=rand(9,1);
+function produceNum(){
+  let arr=[];
+  for(let i=0;i<2;i++){
+    let num=Math.floor((Math.random()*(7-1))+1);
+    if(arr.indexOf(num)==-1){ //不存在推入
+      arr.push(num);  
+    }else{
+      i--; //存在了使i-1增加一次循环次数
+    }
+  }
+  return arr;
+}
+function produceNum1(){
+  let arr1=[];
+  for(let i=0;i<2;i++){
+    let num=Math.floor((Math.random()*(9-1))+1);
+    if(arr.indexOf(num)==-1){ //不存在推入
+      arr.push(num);  
+    }else{
+      i--; //存在了使i-1增加一次循环次数
+    }
+  }
+  return arr1;
+}
+document.getElementById("demo").innerHTML=arr[0];
+document.getElementById("demo1").innerHTML=arr[1];
+document.getElementById("demo2").innerHTML=arr[0];
+document.getElementById("demo3").innerHTML=arr[1];
 if(rand(7,1)>3){
 document.getElementById("if").innerHTML="后";
 }else{
