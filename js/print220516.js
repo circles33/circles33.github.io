606426f8fb31b7fe11bbb8001c314aec7d91a617
circles33 <51255072+circles33@users.noreply.github.com>
2022-06-09 21:16:51 +0800

    Update print220516.js

--- js/print220516.js
+++ js/print220516.js
@@ -1,10 +1,10 @@
 function rand(n,m){
 return Math.floor(Math.random()*(n-m)+m);
 };
-function produceNum(){
+function produceNum(n,m){
   let arr=[];
   for(let i=0;i<2;i++){
-    let num=Math.floor((Math.random()*(7-1))+1);
+    let num=Math.floor((Math.random()*(n-m))+m);
     if(arr.indexOf(num)==-1){ //不存在推入
       arr.push(num);  
     }else{
@@ -25,7 +25,7 @@ function produceNum1(){
   }
   return arr1;
 }
-produceNum();
+produceNum(7,1);
 produceNum1();
 document.getElementById("demo").innerHTML=rand(7,1);
 document.getElementById("demo1").innerHTML=rand(7,1);
@@ -36,4 +36,5 @@ document.getElementById("if").innerHTML="后";
 }else{
 document.getElementById("if").innerHTML="前";
 }
+document.getElementById("demo3").innerHTML=arr[1];
 
