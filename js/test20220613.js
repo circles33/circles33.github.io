54c10d6c45f5ce4f775247d48e682261a2691bcc
circles33 <51255072+circles33@users.noreply.github.com>
2022-06-22 14:12:28 +0800

    Update test20220613.js

--- js/test20220613.js
+++ js/test20220613.js
@@ -7,7 +7,7 @@ i++;
 }
 }
 
-document.getElementById("demo1").innerHTML = demo1();
+document.getElementById("demo11").innerHTML = demo1();
 
 var sum1=0;
         var i1=1;
@@ -15,10 +15,10 @@ var sum1=0;
             sum1+=i1;
             i1++;
         } while (i1<=100);
-document.getElementById("demo2").innerHTML = sum1;
+document.getElementById("demo12").innerHTML = sum1;
 
 var maney=10000;
         for (int i2=1;i2<=5;i++) {
             maney=maney*1.003;
         }
-document.getElementById("demo3").innerHTML = maney;
+document.getElementById("demo13").innerHTML = maney;
