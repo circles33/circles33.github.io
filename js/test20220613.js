d15ac0ba1d926dd21ef69a72ef66759b7f85f6e1
circles33 <51255072+circles33@users.noreply.github.com>
2022-07-02 16:08:44 +0800

    Update test20220613.js

--- js/test20220613.js
+++ js/test20220613.js
@@ -1,15 +1,4 @@
 function myFunction(){
-	var day;
-        var d = new Date().getDay();
-        switch(d){
-        case 0:
-        day="周一";
-        break;
-        case 5:
-        day ="周六";
-        break;
-        case 6:
-        day ="周天";
-        }
-	document.getElementById("demo").innerHTML="明天是"+day;
+	alert("疏散通道");
+	//document.getElementById("demo").innerHTML="明天是"+day;
 }
