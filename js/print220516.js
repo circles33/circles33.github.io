748dcc8a36d72eecd67b07463a48b91790a86d22
circles33 <51255072+circles33@users.noreply.github.com>
2022-07-04 17:06:03 +0800

    Update print220516.js

--- js/print220516.js
+++ js/print220516.js
@@ -30,7 +30,7 @@ produceNum1();
 document.getElementById("demo").innerHTML=rand(4,1);
 document.getElementById("demo1").innerHTML=rand(8,1);
 
-document.getElementById("demo2").innerHTML=rand(12,1);}
+document.getElementById("demo2").innerHTML=rand(12,1);
 
 document.getElementById("demo3").innerHTML=rand(26,1);
 
