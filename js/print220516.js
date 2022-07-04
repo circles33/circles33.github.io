f896261c30f2aea8c4793073540df84a45204992
circles33 <51255072+circles33@users.noreply.github.com>
2022-07-04 16:59:14 +0800

    Update print220516.js

--- js/print220516.js
+++ js/print220516.js
@@ -36,7 +36,7 @@ document.getElementById("demo3").innerHTML=rand(26,1);
 }
 if(rand(4,1)=1){
 document.getElementById("if").innerHTML="general part";
-}else if{
+}else if(rand(4,1)=2){
 document.getElementById("if").innerHTML="3 part";
 }
 else{
