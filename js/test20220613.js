37d5963f5133211768b845bc60786403286681f6
circles33 <51255072+circles33@users.noreply.github.com>
2022-07-06 15:51:05 +0800

    Update test20220613.js

--- js/test20220613.js
+++ js/test20220613.js
@@ -1,4 +1,8 @@
 function myFunction(){
-	var person = {name:"公交车",age:5}
+	function Fun(name,age) = {
+this.name=name;
+this.age=age;
+}
+let person = new Fun("华为",20);
 	document.getElementById("demo").innerHTML=person.name+"年龄是"+person.age;
 }
