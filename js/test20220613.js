9bf732b5e94ea813529989913816a44eef15ddf7
circles33 <51255072+circles33@users.noreply.github.com>
2022-07-06 16:05:53 +0800

    Update test20220613.js

--- js/test20220613.js
+++ js/test20220613.js
@@ -3,6 +3,6 @@ function myFunction(){
 this.name=name;
 this.age=age;
 }
-let person = new Fun("华为",20);
+let person = new Fun(华为,20);
 	document.getElementById("demo").innerHTML=person.name+"年龄是"+person.age;
 }
