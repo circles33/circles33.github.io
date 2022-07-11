b48995db37b573cd69d02134069fefa21c28c444
circles33 <51255072+circles33@users.noreply.github.com>
2022-07-11 21:20:10 +0800

    Update test20220613.js

--- js/test20220613.js
+++ js/test20220613.js
@@ -1,4 +1,8 @@
-function myFunction(){
-	var arr = new Array("10","20","30");
-	document.getElementById("demo").innerHTML=arr[1];
-}
+var w = window.innerWidth
+||document.documentElement.clientWidth
+||document.body.clientWidth;
+var h = window.innerHeight
+||document.documentElement.clientHeight
+||document.body.clientHeight;
+var x = document.getElementById('demo');
+x.innerHTML= "浏览器内窗宽度:" + w +",高度:" + h +"。";
