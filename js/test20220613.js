87632aec528e5b74198783219a02a74f7b8e207f
circles33 <51255072+circles33@users.noreply.github.com>
2022-07-12 14:35:08 +0800

    Update test20220613.js

--- js/test20220613.js
+++ js/test20220613.js
@@ -1,8 +1,1 @@
-var w = window.innerWidth
-||document.documentElement.clientWidth
-||document.body.clientWidth;
-var h = window.innerHeight
-||document.documentElement.clientHeight
-||document.body.clientHeight;
-var x = document.getElementById('demo');
-x.innerHTML= "浏览器内窗宽度:" + w +",高度:" + h +"。";
+document.getElementById('demo').innerHTML="本页面的完整URL:</br>"+window.location.href;
