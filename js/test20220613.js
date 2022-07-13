c34a971108798dd58644c09600069405bc688aaf
circles33 <51255072+circles33@users.noreply.github.com>
2022-07-13 17:19:54 +0800

    Update test20220613.js

--- js/test20220613.js
+++ js/test20220613.js
@@ -1,3 +1,3 @@
 function newDoc(){
-window.location.assign("https://www.w3school.com.cn/tiy/t.asp?f=eg_js_location_assign");
+window.location.assign("https://www.w3school.com.cn/tiy/t.asp?f=eg_js_location_assign")
 }
