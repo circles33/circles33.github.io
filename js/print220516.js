d06026a7a1e71853cbeafc9aa2b376026f68cec0
circles33 <51255072+circles33@users.noreply.github.com>
2022-08-30 21:39:40 +0800

    Update print220516.js

--- js/print220516.js
+++ js/print220516.js
@@ -25,6 +25,30 @@ function produceNum1(n,m){
   }
   return arr1;
 }
+function produceNum2(n,m){
+  let arr2=[];
+  for(let i=0;i<2;i++){
+    let num=Math.floor((Math.random()*(n-m))+m);
+    if(arr2.indexOf(num)==-1){ //不存在推入
+      arr2.push(num);  
+    }else{
+      i--; //存在了使i-1增加一次循环次数
+    }
+  }
+  return arr2;
+}
+function produceNum3(n,m){
+  let arr3=[];
+  for(let i=0;i<2;i++){
+    let num=Math.floor((Math.random()*(n-m))+m);
+    if(arr3.indexOf(num)==-1){ //不存在推入
+      arr3.push(num);  
+    }else{
+      i--; //存在了使i-1增加一次循环次数
+    }
+  }
+  return arr2;
+}
 produceNum(7,1);
 produceNum1();
 document.getElementById("demo").innerHTML=rand(4,1);
@@ -44,6 +68,8 @@ document.getElementById("if").innerHTML="2 part";
 }
 document.getElementById("demo10").innerHTML=produceNum(7,1);
 document.getElementById("demo11").innerHTML=produceNum1(9,1);
-document.getElementById("demo12").innerHTML=rand(43,1);
+document.getElementById("demo12").innerHTML=rand(3,1);
+document.getElementById("fly1").innerHTML=produceNum1(26,1);
+document.getElementById("fly2").innerHTML=produceNum1(8,1);
 
 
